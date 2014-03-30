537aef866d47b319285b2892353b47c944732465
Mike Bloy <mike@bloy.org>
2014-03-29 21:26:42 -0500

    use a template to render and a skin

--- contact/app.py
+++ contact/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = "Development Secret Key"
 
 
 @app.route('/')
 def hello_world():
-    return "Hello world!"
+    skin = 'skin_development.html'
+    return render_template('contact.html', skin=skin)
 
 
 if __name__ == '__main__':
