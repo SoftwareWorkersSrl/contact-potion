1e5c10272cd7202067425911b29676f7800c646a
Mike Bloy <mike@bloy.org>
2014-03-29 22:31:21 -0500

    basic form

--- contact/app.py
+++ contact/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+from forms import ContactForm
 
 app = Flask(__name__)
 app.secret_key = "Development Secret Key"
 
 
 @app.route('/')
-def hello_world():
+def contact():
     skin = 'skin_development.html'
-    return render_template('contact.html', skin=skin)
+    form = ContactForm()
+    return render_template('contact.html', skin=skin, form=form)
 
 
 if __name__ == '__main__':
---
+++ contact/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import Form
+from wtforms.fields import (
+    TextField,
+    TextAreaField,
+    SubmitField
+)
+from wtforms.validators import DataRequired, Email
+
+
+class ContactForm(Form):
+    name = TextField("Name", validators=[DataRequired()])
+    email = TextField("Email", validators=[DataRequired(), Email()])
+    subject = TextField("Subject", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send")
