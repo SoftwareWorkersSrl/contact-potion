5fad4dd98b23a8a481ba34c321b92151a85e7073
Mike Bloy <mike@bloy.org>
2014-03-30 09:29:05 -0500

    better error messages

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import Form
+from wtforms.fields.html5 import EmailField
 from wtforms.fields import (
     TextField,
     TextAreaField,
@@ -8,8 +9,15 @@ from wtforms.validators import DataRequired, Email
 
 
 class ContactForm(Form):
-    name = TextField("Name", validators=[DataRequired()])
-    email = TextField("Email", validators=[DataRequired(), Email()])
-    subject = TextField("Subject", validators=[DataRequired()])
-    message = TextAreaField("Message", validators=[DataRequired()])
+    name = TextField("Name",
+                     validators=[DataRequired("Please enter your name")])
+    email = EmailField("Email",
+                       validators=[
+                           DataRequired("Please enter your email address"),
+                           Email("Please enter a valid email address")])
+    subject = TextField("Subject",
+                        validators=[DataRequired(
+                            "Please enter a subject line for the message")])
+    message = TextAreaField("Message",
+                            validators=[DataRequired("Please enter a message")])
     submit = SubmitField("Send")
