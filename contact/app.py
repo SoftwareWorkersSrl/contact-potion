154d4712286695df3bf8960ba77a02d87f9f9d7b
Mike Bloy <mike@bloy.org>
2014-03-30 20:30:18 -0500

    send mail

--- contact/app.py
+++ contact/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request
+from flask.ext.mail import Message, Mail
 from forms import ContactForm
 
 
 app = Flask(__name__)
 app.secret_key = "Development Secret Key"
+app.config['MAIL_SERVER'] = 'localhost'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_RECIPIENT'] = 'test@example.com'
+app.config['MAIL_SENDER'] = 'webmaster@example.com'
 
+mail = Mail()
+mail.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def contact():
@@ -12,8 +20,20 @@ def contact():
     form = ContactForm()
     if request.method == 'POST':
         if form.validate():
-            return "valid Form posted"
+            send_message(form)
+            return 'Message sent'
     return render_template('contact.html', skin=skin, form=form)
+
+
+def send_message(form):
+    message = Message(form.subject.data,
+                      sender=app.config['MAIL_SENDER'],
+                      recipients=['mike@bloy.org'])
+    message.body = """
+    Website contact message from {0} ({1}):
+
+    {2}""".format(form.name.data, form.email.data, form.message.data)
+    mail.send(message)
 
 
 if __name__ == '__main__':
