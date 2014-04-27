86043e6569e2aedc39d28e339ad16b2ef08d2734
Mike Bloy <mike@bloy.org>
2014-04-27 16:20:30 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='contact_potion',
+    version='1.0',
+    long_descripton=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Fabric==1.8.3',
+        'Flask==0.10.1',
+        'Flask-WTF==0.9.5',
+        'Flask-Mail==0.9.0',
+        'gunicorn==18.0',
+        'honcho==0.5.0'
+    ]
+)
